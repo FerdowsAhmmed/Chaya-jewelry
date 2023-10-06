58b7b77539024329e47256c509f15ad23abab001
Ferdows Ahmmed <ferdows962@gmail.com>
2023-10-06 23:35:44 +0600

    firebase installed and site deployed

--- src/Components/AllProducts.jsx
+++ src/Components/AllProducts.jsx
@@ -21,16 +21,33 @@ const AllProducts = () => {
       });
   }, []);
 
+  const [hoveredProductId, setHoveredProductId] = useState(null);
+
   return (
     <div className="my-10">
       <h1 className="text-2xl font-bold pl-10 py-4">All Products</h1>
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-4 px-4">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-10 px-4">
         {products.map((product) => (
-          <div key={product.id} className="border border-solid border-stone-950 p-2 text-left">
-            <img src={product.image} alt={product.name} />
-            <h2 className="font-bold mb-2">{product.name}</h2>
+          <div
+            key={product.id}
+            className="relative bg-white rounded-xl border border-solid border-stone-950 p-2 text-left"
+            onMouseEnter={() => setHoveredProductId(product.id)}
+            onMouseLeave={() => setHoveredProductId(null)}
+          >
+            <img src={product.image} alt={product.name} className="rounded-xl" />
+            <h2 className="font-bold my-2">{product.name}</h2>
             <p className="font-semibold mb-2">{product.description}</p>
             <p className="text-indigo-900 text-xl text-center">Price: ${product.price}</p>
+            <button
+              className={`bg-indigo-600 text-white px-4 py-2 mt-2 rounded-md hover:bg-indigo-800 transition-transform transform ${
+                hoveredProductId === product.id ? "translate-y-0 opacity-100" : "translate-y-5 opacity-0"
+              }`}
+              onClick={() => {
+                // Handle adding the product to the cart here
+              }}
+            >
+              Add to Cart
+            </button>
           </div>
         ))}
       </div>
